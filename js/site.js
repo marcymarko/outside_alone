2765825c5a06734bf5aa05a4004b9fb70cd604e0
marcymarko <82332440+marcymarko@users.noreply.github.com>
2022-02-16 21:56:31 -0500

    init

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-// Call Hello World
-function helloWorld(){
-    alert("hello World");
+//Get the string from the page
+//Controller function
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+//Reverse the string
+//Logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+
+    return revString;
+
+}
+//Display the reversed string to the user
+//View function
+function displayString(revString){
+
+    //Write to the page
+    document.getElementById("message").innerHTML = `Your string reversed is: ${revString}`;
+    //show the alert
+    document.getElementById("alert").classList.remove("invisible");
 }
